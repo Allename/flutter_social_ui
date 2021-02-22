6f28999b55caa4172a8c86650cafa6dc45aed730
Allename <allenameanthony2000@gmail.com>
2021-02-22 20:37:37 +0100

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: <Widget>[
+              Image(
+                height: MediaQuery.of(context).size.height / 2.5,
+                image: AssetImage('assets/images/login_background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
