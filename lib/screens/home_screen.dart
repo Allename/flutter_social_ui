e6f008c38d871edcaa5a8f2ff6b2be4202a4c278
Allename <allenameanthony2000@gmail.com>
2021-02-23 02:41:41 +0100

    feat: update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_social_ui/data/data.dart';
 import 'package:flutter_social_ui/models/user_model.dart';
+import 'package:flutter_social_ui/widgets/following_users.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -50,55 +51,7 @@ class _HomeScreenState extends State<HomeScreen>
       ),
       body: ListView(
         children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
-                child: Text(
-                  'Following',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                  ),
-                ),
-              ),
-              Container(
-                height: 80.0,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: users.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    User user = users[index];
-                    return Container(
-                      margin: EdgeInsets.all(10.0),
-                      height: 60.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            offset: Offset(0, 2),
-                            blurRadius: 6.0,
-                          ),
-                        ],
-                      ),
-                      child: ClipOval(
-                        child: Image(
-                          height: 60.0,
-                          width: 60.0,
-                          image: AssetImage(user.profileImageUrl),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
+          FollowingUsers(),
         ],
       ),
     );
