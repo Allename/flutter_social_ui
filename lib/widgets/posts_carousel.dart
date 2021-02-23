ddb94b6aa536930c4ca6b058d926b517aadb6f6d
Allename <allenameanthony2000@gmail.com>
2021-02-23 04:06:41 +0100

    feat: update

--- lib/widgets/posts_carousel.dart
+++ lib/widgets/posts_carousel.dart
@@ -8,20 +8,64 @@ class PostsCarousel extends StatelessWidget {
 
   PostsCarousel({this.pageController, this.title, this.posts});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
-        child: Text(
-          'Posts',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
+  _buildPost(BuildContext context, int index) {
+    Post post = posts[index];
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(0, 2),
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image(
+              height: 400.0,
+              width: 300.0,
+              image: AssetImage(post.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-      ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+            ),
+          ),
+        ),
+        Container(
+          height: 400.0,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: posts.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildPost(context, index);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
