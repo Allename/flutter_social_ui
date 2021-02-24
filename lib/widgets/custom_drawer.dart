b0ab96023a48e0b2cbbbb0961b17f6f538600883
Allename <allenameanthony2000@gmail.com>
2021-02-24 11:54:05 +0100

    feat: update

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
+      child: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Image(
+                height: 200.0,
+                width: double.infinity,
+                image: AssetImage(currentUser.backgroundImageUrl),
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                bottom: 20.0,
+                left: 20.0,
+                child: Row(
+                  children: <Widget>[
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
